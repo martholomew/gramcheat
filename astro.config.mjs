16fb2df65501537b52b4633bcb50923cbd12ae69
martholomew <martholomew@users.noreply.github.com>
2024-10-18 14:33:16 -0600

    site deploying things

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://martholomew.github.io',
+	base: 'gramcheat',
 	integrations: [
 		starlight({
 			title: 'My Docs',
